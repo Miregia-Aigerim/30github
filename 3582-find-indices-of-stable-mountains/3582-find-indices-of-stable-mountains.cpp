e7c7cccab75672393a0a9cc23510c3d8b7cf4725
Miregia-Aigerim <aigesha031018@gmail.com>
2025-03-10 21:16:41 +0600

    Time: 0 ms (100%), Space: 28.3 MB (20.08%) - LeetHub

---
+++ 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        vector<int> ans;
+        for(int i=0;i<height.size();i++){
+            if(i!=0 && height[i-1]>threshold){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
