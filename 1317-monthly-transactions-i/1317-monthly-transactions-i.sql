fb245d1c720b27aa93ccc2c8fe20a6354d023890
Miregia-Aigerim <aigesha031018@gmail.com>
2025-04-16 11:39:52 +0600

    Time: 629 ms (72.65%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,11 +1,9 @@
 SELECT 
-    LEFT(trans_date, 7) AS month,
-    country, 
-    COUNT(id) AS trans_count,
-    SUM(state = 'approved') AS approved_count,
-    SUM(amount) AS trans_total_amount,
-    SUM((state = 'approved') * amount) AS approved_total_amount
-FROM 
-    Transactions
-GROUP BY 
-    month, country;
+    DATE_FORMAT(trans_date, '%Y-%m') AS month
+    , country
+    , COUNT(*) AS trans_count
+    , SUM(IF(state = 'approved', 1, 0)) AS approved_count
+    , SUM(amount) AS trans_total_amount
+    , SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
